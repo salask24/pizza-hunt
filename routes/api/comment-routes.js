d2867564220b88650897048d112b57c11e513c7c
Kimberly Salas <salask0104@gmail.com>
2021-07-24 07:25:24 -0700

    added timestamp for new reply in single-pizza js

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -15,4 +15,14 @@ router.route('/:pizzaId').post(addComment);
 // /api/comments/<pizzaId>/<commentId>
 router.route('/:pizzaId/:commentId').delete(removeComment);
 
+router
+    .route('/:pizzaId/:commentId')
+    .put(addReply)
+    .delete(removeComment);
+
+
+
+
+    router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
+
 module.exports = router;
