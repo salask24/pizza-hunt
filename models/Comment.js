497abad33bd68e81118617574c3e0188943b1745
Kimberly Salas <salask0104@gmail.com>
2021-07-23 19:35:39 -0700

    added reply schema to comment model

--- models/Comment.js
+++ models/Comment.js
@@ -1,5 +1,22 @@
 const { Schema, model } = require('mongoose');
 
+
+const ReplySchema = new Schema(
+    {
+        replyBody: {
+            type: String
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    }
+);
+
 const CommentSchema = new Schema({
     writtenBy: {
         type: String
